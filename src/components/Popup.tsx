a7d23382669f741fe9572cb7db1604115b9e9dc4
Laszlo Moczo <laszlo.moczo@bitrise.io>
2025-08-11 20:40:47 +0200

    Backup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { RemoveScroll } from "react-remove-scroll";
+import bacik from '../images/rosszbaci-per-jobaci.png';
+import iksz from '../images/iksz.png';
 
 const Popup = () => {
     const [visible, setVisible] = useState(window.sessionStorage.getItem('isPopupVisible') !== 'false');
@@ -34,6 +36,7 @@ const Popup = () => {
                 <div className="popup">
                     <h2>VÁLOGATHAT-E EGY SZÁJVÍZ?</h2>
                     <h3>TEHET-E KÜLÖNBSÉGET KÁROS ÉS HASZNOS BAKTÉRIUMOK KÖZÖTT?</h3>
+                    <img src={bacik} width="164" height="81" />
                     <p>
                         Mint kiderült, igen!<br />
                         Arról a különleges és előnyös jelenségről van szó, hogy a tisztán klór-dioxid alapú szájvíz<br />
@@ -45,7 +48,9 @@ const Popup = () => {
                         A világpiacon jelenleg egyetlen ilyen, tisztán klór-dioxid alapú szájvíz kapható: a Solumium.<br />
                     </p>
                     <a href="https://chemrxiv.org/engage/chemrxiv/article-details/6866954c3ba0887c337e1078" target="_blank" rel="noopener noreferrer">https://chemrxiv.org/engage/chemrxiv/article-details/6866954c3ba0887c337e1078</a>
-                    <button aria-label='Close popup' onClick={() => onClose()}>Close</button>
+                    <button aria-label='Close popup' onClick={() => onClose()}>
+                        <img src={iksz} width="24" height="25" />
+                    </button>
                 </div>
             </div>
         </RemoveScroll>
